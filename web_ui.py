281379bd376b113c33a4dc6e09911d38ddab4039
Beytullah Yayla <78471151+BeytullahYayla@users.noreply.github.com>
2024-08-12 15:40:38 +0300

    Update web_ui.py

--- web_ui.py
+++ web_ui.py
@@ -4,10 +4,25 @@ from base64 import b64encode
 from pages import *
 from constants import *
 
+def inject_custom_css():
+    st.markdown(
+        """
+        <style>
+        .css-1v3fvcr edgvbvh9 { /* Sidebar radio button text */
+            font-size: 20px !important; /* Adjust the size as needed */
+        }
+        .css-1v3fvcr { /* Sidebar radio button */
+            min-height: 50px !important; /* Adjust the size as needed */
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
 def sidebar():
-    selection = st.sidebar.radio('Go to', ['Portfolio', 'Realtime Customer Counting', 'DentAI','Drivable Area Segmentation','Turtlebot3 Ros Object Detection','Behavioral Clonning Model'])
-   
+    selection = st.sidebar.radio('Go to', 
+                                 ['Portfolio', 'Realtime Customer Counting', 'DentAI', 'Drivable Area Segmentation', 'Turtlebot3 Ros Object Detection', 'Behavioral Clonning Model'])
+
     if selection == 'Portfolio':
         web_portfolio()
     elif selection == 'Realtime Customer Counting':
@@ -20,9 +35,10 @@ def sidebar():
         turtlebot3_object_detection()
     elif selection == 'Behavioral Clonning Model':
         self_driving_car_behavioral_clonning()
-        
+
 def main():
-    st.set_page_config(page_title = "Beytullah Yayla's Portfolio",page_icon="⭐")
+    st.set_page_config(page_title="Beytullah Yayla's Portfolio", page_icon="⭐")
+    inject_custom_css()  # Inject the custom CSS
     sidebar()
 
 if __name__ == '__main__':
